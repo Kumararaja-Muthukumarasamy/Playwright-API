a8ff1957fffdcef74b7743046cfcb3631c3b0029
Kumar <reach.mdkumar@gmail.com>
2026-03-26 21:08:18 +0530

    Secrets setup for credentials

--- config/env.ts
+++ config/env.ts
@@ -3,8 +3,10 @@ const env = (process.env.ENV || 'qa') as keyof typeof configMap;
 const configMap = {
   qa: {
     baseUrl: 'https://dummyjson.com',
-    username: 'emilys',
-    password: 'emilyspass',
+    // username: 'emilys',
+    // password: 'emilyspass',
+    username: process.env.APP_USERNAME || '',
+    password: process.env.APP_PASSWORD || '',
     tokenExpiry: 30
   },
   dev: {
