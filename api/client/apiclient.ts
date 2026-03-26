08d02d38b122b28d6474ac17dd8c6dd99e0d21a1
Kumar <reach.mdkumar@gmail.com>
2026-03-26 22:53:05 +0530

    Implemented Allure reports

--- api/client/apiclient.ts
+++ api/client/apiclient.ts
@@ -1,6 +1,6 @@
 import { getToken } from "../auth/tokenmanager";
 import { HEADERS } from '../constants/headers';
-
+import * as allure from 'allure-js-commons';
 
 export class APIClient {
     constructor(private request: any) { }
@@ -9,21 +9,46 @@ export class APIClient {
 
         const headers: Record<string, string> = {};
 
-        // ✅ only add token if auth is NOT false
         if (options?.auth !== false) {
             const token = await getToken(this.request);
-         headers[HEADERS.AUTHORIZATION] = `Bearer ${token}`;
+            headers[HEADERS.AUTHORIZATION] = `Bearer ${token}`;
         }
 
-        return await this.request.get(path, { headers });
-    }
+        return await allure.step(`GET ${path}`, async () => {
 
-    async post(path: string, data: any) {
-        const token = await getToken(this.request);
-        return await this.request.post(path, {
-            data,
-            headers: {
-                Authorization: `Bearer ${token}`
+            try {
+                const response = await this.request.get(path, { headers });
+                const body = await response.json();
+
+                await allure.attachment(
+                    'Request',
+                    JSON.stringify({ method: 'GET', path, headers }, null, 2),
+                    'application/json'
+                );
+
+                await allure.attachment(
+                    'Response',
+                    JSON.stringify(body, null, 2),
+                    'application/json'
+                );
+
+                await allure.attachment(
+                    'Status Code',
+                    response.status().toString(),
+                    'text/plain'
+                );
+
+                return response;
+
+            } catch (error: any) {
+
+                await allure.attachment(
+                    'Error',
+                    JSON.stringify(error.message || error, null, 2),
+                    'text/plain'
+                );
+
+                throw error; // ❗ IMPORTANT
             }
         });
     }
--- playwright.config.ts
+++ playwright.config.ts
@@ -4,26 +4,27 @@ import { config } from './config/env';
 
 export default defineConfig({
   testDir: './tests',
- 
+
   fullyParallel: true,
-  
+
   forbidOnly: !!process.env.CI,
- 
+
   retries: process.env.CI ? 0 : 0,
-  
+
   workers: process.env.CI ? 2 : undefined,
-  
+
   reporter: [
     ['list'],
-    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['allure-playwright']
   ],
- 
-   use: {
+
+  use: {
     baseURL: config.baseUrl,
     trace: 'on-first-retry',
   },
 
-   projects: [
+  projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
